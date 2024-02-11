13e725c1d035646fd40ac9b9930af7221e57f686
prasannapoudel33 <hero.prasannap2@gmail.com>
2024-02-11 21:25:39 +0545

    jpg error fixed

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -11,19 +11,27 @@ root.geometry("700x500")
 root.configure(bg="#36454F")
 
 filename = None  # Define filename as a global variable
-
 def showimage():
     global filename
     file = filedialog.askopenfile(initialdir=os.getcwd(),
                                   title='Select Image File',
                                   filetype=(("PNG file", "*.png"),
                                             ("JPG File", "*.jpg"),
-                                            ("All file", "*.txt")))
+                                            ("All file", "*.*")))  # Changed "*.txt" to "*.*" to allow any file type
     filename = file.name  # Store the file name
     img = Image.open(filename)
+
+    # Convert JPEG to PNG if necessary
+    if filename.lower().endswith('.jpg') or filename.lower().endsWith('.jpeg'):
+        temp_filename = os.path.splitext(filename)[0] + '.png'  # Change file extension to .png
+        img = img.convert('RGB')  # Convert image to RGB mode if it's not
+        img.save(temp_filename)
+        filename = temp_filename  # Update filename to the new PNG file
+
     img = ImageTk.PhotoImage(img)
     lbl.configure(image=img, width=250, height=250)
     lbl.img = img
+
 
 def Hide():
     global filename
@@ -33,9 +41,13 @@ def Hide():
 
 def Show():
     global filename
-    clear_message = lsb.reveal(filename)
-    text1.delete(1.0, END)  # Corrected from text.delete() to text1.delete()
-    text1.insert(END, clear_message)
+    try:
+        clear_message = lsb.reveal(filename)
+        text1.delete(1.0, END)
+        text1.insert(END, clear_message)
+    except IndexError as e:
+        text1.delete(1.0, END)
+        text1.insert(END, "No hidden message found or message cannot be decoded.")
 
 def reset():
     global filename
@@ -48,9 +60,13 @@ def save():
     global secret
     if secret:
         filename_parts = os.path.splitext(filename)
+        # Check if the image mode is 'RGBA' and convert it to 'RGB'
+        if secret.mode == 'RGBA':
+            secret = secret.convert('RGB')
         new_filename = f"{filename_parts[0]}_stegno{filename_parts[1]}"
         secret.save(new_filename)
         reset()  # Reset the image and text after saving
+
 
 # ICONS
 image_icon = PhotoImage(file="logo.jpg")
